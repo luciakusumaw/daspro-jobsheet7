9b34333fe3d6c8a051893a29d1b65fc774658a8f
luciakusumaw <itsmelucia27@gmail.com>
2025-11-01 10:02:46 +0700

    Question 1

--- SiakadFor13.java
+++ SiakadFor13.java
@@ -3,6 +3,8 @@ public class SiakadFor13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double score = 0, lowest = 100, highest = 0;
+        int passCount = 0, failCount = 0;
+
         for (int i = 1; i <= 10; i++) {
             System.out.print("Enter the score for student-" + i + ":");
             score = sc.nextDouble();
@@ -12,9 +14,13 @@ public class SiakadFor13 {
             if (score < lowest) {
                 lowest = score;
             }
+            if (score >= 60) passCount++;
+            else failCount++;
         }
         System.out.println("Highest score: " + highest);
         System.out.println("Lowest score: " + lowest);
+        System.out.println("Passed: " + passCount);
+        System.out.println("Failed: " + failCount);
         sc.close();
     }
 }
---
+++ SiakadWhile13.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SiakadWhile13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score, amt, i = 0;
+        System.out.println("Enter the Amount of Student:  ");
+        amt = sc.nextInt();
+        while (i < amt) {
+            i++;
+        }
+    }
+}
